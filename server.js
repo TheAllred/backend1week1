d3b92693c467fb0ac59fe81dda46a6d0d0bfeae5
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-05-27 22:32:56 -0600

    w3 wip

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const baseController = require("./controllers/baseController");
+
+/* ***********************
+ *View Engine and Templates
+ *************************/
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(require("./routes/static"))
+app.use(require("./routes/static"));
+
+// index route
+app.get("/", baseController.buildHome);
+// custom route
+app.get("/custom", function (req, res) {
+  res.render("custom", { title: "Custom" });
+});
+// index route
+app.get("/sedan", function (req, res) {
+  res.render("sedan", { title: "Sedans" });
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
